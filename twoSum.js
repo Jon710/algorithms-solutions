de0134c45cad1fe37b8e1ac1f038502f05885d6a
João Luis Moraes <jonmoraesnl@gmail.com>
2021-05-03 12:20:43 -0300

    first commit

---
+++ twoSum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // lookup speed in map is constant O(1)
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    currentValue = nums[i];
+    if (map.has(currentValue)) {
+      return [map.get(currentValue), i];
+    }
+
+    let difference = target - currentValue;
+    map.set(difference, i);
+  }
+};
+
+twoSum([1, 2, 5], 7);
