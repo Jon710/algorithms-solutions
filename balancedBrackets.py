63ed7ef8646c685d14151db656717b25d616cd5b
João Luis Moraes <jonmoraesnl@gmail.com>
2021-05-23 15:09:16 -0300

    balanced brackets  - hackerrank

---
+++ balancedBrackets.py
@@ -0,0 +1,21 @@
+def isBalanced(s):
+    stack = []
+
+    bracket = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
+
+    for char in s:
+        if char in ['{', '[', '(']:
+            stack.append(char)
+        else:
+            if stack:
+                top = stack.pop()
+                if bracket[top] != char:
+                    return 'NO'
+            else:
+                return 'NO'
+
+    return 'NO' if stack else 'YES'
