5438cc2cd8d694cbb2ef43b9f55b569a535c4257
João Luis Moraes <jonmoraesnl@gmail.com>
2021-07-18 23:39:51 -0300

    IsUnique

---
+++ CTCI/src/IsUnique.java
@@ -0,0 +1,28 @@
+// ASCII has 256 characters at most. Unicode is 4bytes, extra space would be needed.
+
+import java.util.HashSet;
+
+public class IsUnique {
+    boolean hasUniqueCharacters(String str) {
+        HashSet<Character> mySet = new HashSet(256);
+
+        if (str.length() > 256) return false;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (mySet.contains(str.charAt(i))) {
+                return false;
+            } else {
+                mySet.add(str.charAt(i));
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        IsUnique iu = new IsUnique();
+
+        System.out.println(iu.hasUniqueCharacters("abcder"));
+        System.out.println(iu.hasUniqueCharacters("joao"));
+        System.out.println(iu.hasUniqueCharacters("palmeiras"));
+    }
+}
