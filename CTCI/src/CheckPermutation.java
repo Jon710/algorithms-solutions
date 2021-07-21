3b319032708098cb7494cfcc9c88b368f7ff9af0
João Luis Moraes <jonmoraesnl@gmail.com>
2021-07-21 12:07:36 -0300

    CheckPermutation

--- CTCI/src/CheckPermutation.java
+++ CTCI/src/CheckPermutation.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class CheckPermutation {
+import java.util.Arrays;
+
+// Given 2 strings, write a method to decide if one is a permutation of the other.
+public class CheckPermutation {
+    public boolean checkPermutation(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+        return sort(str1).equals(sort(str2));
+    }
+
+    public static String sort(String s) {
+        char[] content = s.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
+    }
+
+    public static void main(String[] args) {
+        CheckPermutation c = new CheckPermutation();
+
+        System.out.println(c.checkPermutation("aabsd", "aba"));
+        System.out.println(c.checkPermutation("joao", "oaoj"));
+        System.out.println(c.checkPermutation("palmeiras", "porco"));
+    }
 }
